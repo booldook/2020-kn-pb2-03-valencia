05f31fd866a5e93204a37ca798932f22bd0879f3
booldook <booldook@gmail.com>
2020-09-15 11:12:55 +0900

    10.slide

--- public/js/10.slide.js
+++ public/js/10.slide.js
@@ -1,14 +1,37 @@
-var n = 1;
-function ani() {
-	$(".wrapper2 .slide-wrap").stop().animate({"left": -n*100 + "%"}, 500, function(){
-		if(n == 4) {
-			$(this).css("left", 0);
-			n = 1;
-		}
-		else {
-			n++;
-		}
-	});
-}
+// 즉시실행함수 IIFE -Immediately Invoked Fucntion Expression
+(function () {
+	var a = 1;
+	console.log(a);
+	console.log("즉시실행");
+})();
 
-setInterval(ani, 3000);
+// 시험장
+(function(){
+	var n = 0;
+	function ani() {
+		if(n == 3) n = 0;
+		else n++;
+		$(".wrapper1 .slide-wrap").stop().animate({"left": -n*100 + "%"}, 500);
+	}
+	setInterval(ani, 3000);
+})();
+
+
+// 무한루프
+(function(){
+	var n = 1;
+	function ani() {
+		$(".wrapper2 .slide-wrap").stop().animate({"left": -n*100 + "%"}, 500, function(){
+			if(n == 4) {
+				$(this).css("left", 0);
+				n = 1;
+			}
+			else {
+				n++;
+			}
+		});
+	}
+	setInterval(ani, 3000);
+})();
+
+
