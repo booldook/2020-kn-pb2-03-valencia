4e9ba0ea8f9348a428665aa98479444c0259ce8a
booldook <booldook@gmail.com>
2020-09-15 11:31:41 +0900

    wrapper3

--- public/js/10.slide.js
+++ public/js/10.slide.js
@@ -35,3 +35,34 @@
 })();
 
 
+// 시험장 prev, next
+(function(){
+	var n = 0;
+	$(".wrapper3 .btn-prev").click(onPrev);
+	$(".wrapper3 .btn-next").click(onNext);
+	function onPrev() {
+		if(n > 0) n--;
+		ani();
+	}
+	function onNext() {
+		if(n < 3) n++;
+		ani();
+	}
+	function ani() {
+		$(".wrapper3 .slide-wrap").stop().animate({"left": -n*100 + "%"}, 500, pagerToggle);
+	}
+	function pagerToggle(){
+		$(".wrapper3 .pager-wrap").find("span").show();
+		if(n == 0) {
+			$(".wrapper3 .pager-wrap").find("span").eq(0).hide();
+			$(".wrapper3 .pager-wrap").find("span").eq(1).hide();
+		}
+		if(n == 3) {
+			$(".wrapper3 .pager-wrap").find("span").eq(1).hide();
+			$(".wrapper3 .pager-wrap").find("span").eq(2).hide();
+		}
+	}
+	pagerToggle();
+})();
+
+
