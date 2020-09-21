64ef36920f80e762a01478603d16ef8d71e38a9b
booldook <booldook@gmail.com>
2020-09-21 17:38:11 +0900

    animation 정리

--- public/js/index.js
+++ public/js/index.js
@@ -287,3 +287,17 @@ $(".modal-wrap").click(function(e) { e.stopPropagation() });
 
 })();
  
+
+/************ .info-wrapper *************/
+$(".info-wrapper > .info-lt").mouseenter(onInfoLtOver);
+$(".info-wrapper > .info-lt").mouseleave(onInfoLtLeave);
+function onInfoLtOver() {
+	$(this).find(".underline").eq(0).find("div").attr("style", "");
+	$(this).find(".underline").eq(1).find("div").attr("style", "");
+	$(this).find(".underline").eq(0).find("div").css({"animation-delay": "0s", "animation-name": "underline"});
+	$(this).find(".underline").eq(1).find("div").css({"animation-delay": "0.25s", "animation-name": "underline"});
+}
+function onInfoLtLeave() {
+	$(this).find(".underline").eq(0).find("div").css({"transform": "scaleX(1)", "animation-delay": "0.25s", "animation-name": "underline-rev"});
+	$(this).find(".underline").eq(1).find("div").css({"transform": "scaleX(1)", "animation-delay": "0s", "animation-name": "underline-rev"});
+}
